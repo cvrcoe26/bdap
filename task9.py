272449348ed422a5169d317966c48a97cbce58cb
cvrcoe26 <guru02721@gmail.com>
2025-10-29 22:41:02 +0530

    Update task9.py

--- task9.py
+++ task9.py
@@ -7,17 +7,20 @@ from pyspark.sql.functions import to_date, col, avg
 spark = SparkSession.builder.appName('Task9').getOrCreate()
 df = spark.read.csv('/content/employees.csv', header=True, inferSchema = True)
 df.show()
+
 rdd = df.rdd
-rdd.collect()
-rdd.count()
+print(rdd.collect())
+
+print(rdd.count())
+
 df1 = df.withColumn('DOD', to_date(col('HIRE_DATE'), 'dd-MMM-yy'))
 df1_filterd = df1.filter(col('DOD') > '2014-12-31')
 rdd_filterd = df1_filterd.rdd
-rdd_filterd.collect()
+print(rdd_filterd.collect())
 names_df = rdd.map(lambda x: (x['FIRST_NAME'], x['LAST_NAME']))
-names_df.collect()
+print(names_df.collect())
 avg_sal = (rdd.map(lambda x: (x['DEPARTMENT_ID'], (x['SALARY'], 1)))
     .reduceByKey(lambda a, b : (a[0]+b[0], a[1]+b[1]))
     .mapValues(lambda x: x[0]/x[1]))
 
-avg_sal.collect()
+print(avg_sal.collect())
